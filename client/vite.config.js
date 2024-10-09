eb3c9a5fcc75a587a5d720cb9315408ef0383bca
ssoto83 <168875470+ssoto83@users.noreply.github.com>
2024-10-09 01:54:02 -0400

    updated the script to include host

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT, 10) || 3000,
     open: true
   }
 })

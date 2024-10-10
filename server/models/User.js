90063a3c2d4347daecd2f4cc8da62880acb4757e
Zain Ul Abid <bajwazain121@outlook.com>
2024-10-10 19:59:14 -0400

    Created Models
    Updated models for user employee and timeoff

---
+++ server/models/User.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+// User Model
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must match an email address!"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ["admin", "employee"],
+    default: "employee",
+  },
+});
+
+// hash user password
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
+
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+const User = mongoose.model("User", userSchema);
+module.exports = User;

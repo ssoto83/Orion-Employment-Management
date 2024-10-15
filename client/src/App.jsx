a9f99463f83b2f9d42f4a9d4a468fed4acddcb96
ssoto83 <168875470+ssoto83@users.noreply.github.com>
2024-10-15 18:38:52 -0400

    worked in the App.jsx and the main.jsx adding the modal imports, apollo, paths, etc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Outlet, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { useState } from 'react';
+
+import Nav from './components/Nav';
+import Dashboard from './pages/dashboard';
+import EmployeeInfoPage from './pages/employeeInfoPage';
+import EmployeesPage from './pages/employeesPage';
+import LoginPage from './pages/loginPage';
+import TimeOffPage from './pages/timeOffPage';
+
+//Import Modals
+import AddEmployeeModal from './components/addEmployeeModal';
+import EmployeeCredModal from './components/employeeCredModal';
+//import EmployeeSearch from './components/employeeSearch';
+//import TimeOffRequest from './components/timeOffRequest';
+
+// Link to GraphQL
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql', 
+})
+
+// Apollo client
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  const [isAddEmployeeModalOpen, setAddEmployeeModalOpen] = useState(false);
+  const [isEmployeeCredModalOpen, setEmployeeCredModalOpen] = useState(false);
+  //const [isEmployeeSearchModalOpen, setEmployeeSearchModalOpen]= useState(false);
+  //const [isTimeOffRequestModalOpen, setTimeOffRequestModalOpen] = useState(false);
+
+   return (
+    <ApolloProvider client={client}>
+       <Router>
+        <Nav/>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/employee/:id" element={<EmployeeInfoPage />} />
+          <Route path="/employee" element={<EmployeesPage 
+              onAddEmployee={() => setAddEmployeeModalOpen(true)}
+          />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/time-off" element={<TimeOffPage
+          onRequestTimeOff={() => setTimeOffRequestModalOpen(true)}
+          />} />
+        </Routes>
+        
+        <AddEmployeeModal
+            isOpen={isAddEmployeeModalOpen}
+            onClose={() =>setAddEmployeeModalOpen(false)}
+        />
+
+        <EmployeeCredModal
+            isOpen={isEmployeeCredModalOpen}
+            onClose={() =>setEmployeeCredModalOpen(false)}
+        />
+
+        {/* <EmployeeSearch
+            isOpen={isEmployeeSearchModalOpen} 
+            onClose={() => setEmployeeSearchModalOpen(false)} 
+        />
+
+        <TimeOffRequest
+            isOpen={isTimeOffRequestModalOpen}
+            isClose={() => setTimeOffRequestModalOpen(false)}
+        /> */}
+
+        <Outlet />
+       </Router>
+    </ApolloProvider>
   )
 }
 
-export default App
+export default App;

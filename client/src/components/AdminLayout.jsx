33a79209d77bb9dec420e71f5dba2dbe4a58c5de
Zain Ul Abid <bajwazain121@outlook.com>
2024-10-26 11:32:06 -0400

    Updating delete function for employee

--- client/src/components/AdminLayout.jsx
+++ client/src/components/AdminLayout.jsx
@@ -7,8 +7,6 @@ const AdminLayout = () => {
     const menuItems = [
         { name: 'Dashboard', link: '/admin' },
         { name: 'View All Employees', link: '/admin/employees' },
-        { name: 'Add Employee', link: '/admin/add-employee' },
-        { name: 'Update Employee', link: '/admin/update-employee' },
         { name: 'Approve Time Off Requests', link: '/admin/time-off-requests' },
     ];
     return (

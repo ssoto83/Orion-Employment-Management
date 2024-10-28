82df5d1dcc28ce4f93f72f468856910b962bf0e4
ssoto83 <168875470+ssoto83@users.noreply.github.com>
2024-10-27 21:27:17 -0400

    removed some console logs and authentication functions that where not needed

--- server/seeds/cleanDB.js
+++ server/seeds/cleanDB.js
@@ -3,14 +3,13 @@ const cleanDB = async (db) => {
     if (!db) {
       throw new Error("Database connection not provided");
     }
-    console.log("Cleaning database...", db);
 
     const collections = await db.collections;
     // Drop each collection
     for (const key in collections) {
       const collection = collections[key];
       await collection.deleteMany({});
-      console.log(`Cleared collection: ${collection.collectionName}`);
+    
     }
 
     console.log("Database cleaned successfully");
